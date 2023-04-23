1e628b6f31df197a6473535934b0551dcedb711b
Amine Safsafi <senpai.codes@gmail.com>
2023-04-23 06:00:54 +0200

    more fixes

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInitf08171d380845fca75fd30bea961d8a7
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInitf08171d380845fca75fd30bea961d8a7', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitf08171d380845fca75fd30bea961d8a7', 'loadClassLoader'));
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,28 +1,19 @@
 <?php return array(
     'root' => array(
         'name' => 'wp-senpai/senpai-wp-test',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd7bc24254673969c85b1cad797e3523b06cc5ebc',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        'senpai/wp-senpai' => array(
+        'wp-senpai/senpai-wp-test' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '0c839b526d03d36826983924bc774410e6622866',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../senpai/wp-senpai',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
-        'wp-senpai/senpai-wp-test' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'reference' => 'd7bc24254673969c85b1cad797e3523b06cc5ebc',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
