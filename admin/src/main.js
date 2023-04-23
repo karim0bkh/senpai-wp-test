d7bc24254673969c85b1cad797e3523b06cc5ebc
Amine Safsafi <senpai.codes@gmail.com>
2023-04-23 05:14:31 +0200

    Fix Public IP V4

--- admin/src/main.js
+++ admin/src/main.js
@@ -4,7 +4,7 @@
 import Swal from 'sweetalert2';
 window.Swal = Swal;
 
-import 'public-ip';
+import {publicIp, publicIpv4, publicIpv6} from 'public-ip';
 
 import 'js-loading-overlay'
 
@@ -12,10 +12,16 @@ import'./style.css';
 
 jQuery(document).ready(function($){
     console.log('main ready');
-    publicIp.v4().then(function(ipv4){
+
+})
+
+jQuery(document).ready( async function($){
+    console.log('publicIp ready');
+
+    publicIpv4().then(function(ipv4){
         $('#senpai-info-ip-v4').html(ipv4);
     }).catch(e => {
         console.log('There has been a problem with your fetch operation IP V4: ' + e.message);
         $('#senpai-info-ip-v4').html('Not Reachable');
       });
-})
+}) 
